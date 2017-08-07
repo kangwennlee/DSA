c5fcf1b476366d803999cad2584790ee30279f2c
Lee Kang Wenn <kangwenn@gmail.com>
2017-08-07 12:48:12 +0800

    SortedList

--- JavaApplication10/src/ADT/SortedLinkList.java
+++ JavaApplication10/src/ADT/SortedLinkList.java
@@ -12,8 +12,8 @@ package ADT;
  */
 public class SortedLinkList<T extends Comparable<? super T>> implements SortedListInterface<T> {
 
-    private Node firstNode;
-    private int length;
+    private Node firstNode; //first node in the list
+    private int length; //total number of entries in the list
 
     public SortedLinkList() {
         firstNode = null;
@@ -26,22 +26,67 @@ public class SortedLinkList<T extends Comparable<? super T>> implements SortedLi
         length++;
         return true;
     }
-
+    
+    //add newEntry according to sequences, from lower values to higher values
     private Node add(T newEntry, Node currNode) {
+        /*if end of list or newEntry has lower value than currNode, 
+        then the newEntry is added before the currNode
+        */
         if ((currNode == null) || newEntry.compareTo((T) currNode.getData()) <= 0) {
             currNode = new Node(newEntry, currNode);
-        } else {
+        } 
+        //if newEntry has higher value than currNode, then continue to traverse through the list
+        else {
             Node nodeAfter = add(newEntry, currNode.getNext());
             currNode.setNext(nodeAfter);
         }
         return currNode;
     }
 
+    /* the entry will first pass to the getPosition() to retrieve the position of the entry
+       it will then be removed through the remove(givenPosition) function and return true
+       if the list is empty, then the getPosition will return -1 which is a null node, 
+       since the entry does not match with the null node, it will return false*/
     @Override
     public boolean remove(T anEntry) {
         return anEntry == remove(getPosition(anEntry));
     }
 
+    @Override
+    public T remove(int givenPosition) {
+        T result = null;
+        
+        // if the givenPosition to be removed is in valid range 
+        if ((givenPosition >= 1) && (givenPosition <= length)) {
+            // CASE 1: if the position is pointing to the first entry, the data of the first entry will be returned
+            // update the firstNode to be pointing to the next node
+            if (givenPosition == 1) {      	
+                result = (T) firstNode.getData();     	
+                firstNode = firstNode.getNext();		
+            } 
+            /*CASE 2: if position pointing to middle or last node, 
+            traverse through the list till the node before the desired node, 
+            and point this nodeBefore to the node after the desired node,
+            */
+            else {                                                    
+                Node nodeBefore = firstNode;
+                for (int i = 1; i < givenPosition - 1; ++i) {
+                    nodeBefore = nodeBefore.getNext();
+                }
+                result = (T) nodeBefore.getNext().getData();  		
+                nodeBefore.setNext(nodeBefore.getNext().getNext());
+            } 															// one to be deleted (to disconnect node from chain)
+            //for successful removal, the size of the list will be decreased
+            length--;
+        }
+
+        return result;
+    }
+    
+    /*get the position of the entry in the list
+    by traversing the list from the first node until the desired node, retrieve and remove the particular position
+    if the entry is not found in the list, position of -1 is returned
+    */
     @Override
     public int getPosition(T anEntry) {
         Node currNode = firstNode;
@@ -58,6 +103,11 @@ public class SortedLinkList<T extends Comparable<? super T>> implements SortedLi
         return position;
     }
 
+    /*retrieve the entry from the list at the given position
+    it first make sure that the givenPosition is between the valided range(from 1 to length of list)
+    then it will traverse the list by from the first node until the desired node, 
+    retrieve and return the data of the desired node
+    */
     @Override
     public T getEntry(int givenPosition) {
         T result = null;
@@ -73,6 +123,11 @@ public class SortedLinkList<T extends Comparable<? super T>> implements SortedLi
         return result;
     }
 
+    /*check whether the entry exist in the list or not,
+    by traserving the list and compare each node in the list with the entry,
+    it will iterate until the entry is found, and return true
+    or it will return false if it reaches the end of the list without the entry found
+    */
     @Override
     public boolean contains(T anEntry) {
         boolean found = false;
@@ -90,54 +145,22 @@ public class SortedLinkList<T extends Comparable<? super T>> implements SortedLi
         return tempNode != null && tempNode.getData().equals(anEntry);
     }
 
-    @Override
-    public T remove(int givenPosition) {
-        T result = null;
-
-        if ((givenPosition >= 1) && (givenPosition <= length)) {
-            if (givenPosition == 1) {      	// CASE 1: remove first entry
-                result = (T) firstNode.getData();     	// save entry to be removed
-                firstNode = firstNode.getNext();		// update firstNode to point to the next node
-            } else {                         	// CASE 2: remove interior entry or last entry
-                Node nodeBefore = firstNode;
-                for (int i = 1; i < givenPosition - 1; ++i) {
-                    nodeBefore = nodeBefore.getNext();		// advance nodeBefore to its next node
-                }
-                result = (T) nodeBefore.getNext().getData();  	// save entry to be removed	
-                nodeBefore.setNext(nodeBefore.getNext().getNext());	// make node before point to node after the 
-            } 															// one to be deleted (to disconnect node from chain)
-
-            length--;
-        }
-
-        return result;
-    }
-
+    //remove all current entries from the list, update the firstNode to null, and length of list to 0 
     @Override
     public final void clear() {
         firstNode = null;
         length = 0;
     }
-
+    
+    //retrieve the total number of entries from the list
     @Override
     public int getLength() {
         return length;
     }
-
+    
+    //check if the list is empty by checking the total number of entries in the list
     @Override
     public boolean isEmpty() {
         return (length == 0);
     }
-
-    @Override
-    public String toString() {
-        String outputStr = "";
-        Node currentNode = firstNode;
-        while (currentNode != null) {
-            outputStr += currentNode.getData() + "\n";;
-            currentNode = currentNode.getNext();
-        }
-        return outputStr;
-    }
-
 }
